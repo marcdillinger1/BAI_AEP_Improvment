586ad7e1bc384552af82cdcf361fdd8701b90d45
marcdillinger1 <marc.dillinger@students.fhnw.ch>
2024-10-11 00:29:48 +0200

    pizza.py und database.py mit Funktionen erweitert. p.py - Funktion zur anzeige von Informationen der Pizzen, Funktion der überprüfung von Pizzen nach Toppings. db.py - Funktion zur Anzeige des gesamten Pizza menus, Funktion zur suche von Pizzen mit bestimmten Toppings, Funktion zur suche von Pizza mit bestimmten namen

--- database.py
+++ database.py
@@ -14,3 +14,21 @@ pizzas = [
     Pizza("Prosciutto Funghi", ["tomato", "mozzarella", "ham", "mushrooms"], 16.00),
     Pizza("Tonno Cipolla", ["tomato", "mozzarella", "tuna", "onions"], 16.00)
 ]
+
+#Funktion um das gesamte Pizza-Menu anzuzeigen
+
+def pizza_menu():
+    return [pizza.pizza_informations() for pizza in pizzas]
+
+#Funktion um Pizzen mit bestimmten Toppings anzuzeigen
+
+def show_pizza_with_topping (topping):
+    return [pizza.pizza_informations() for pizza in pizzas if pizza.has_topping(topping)]
+
+#Funktion um eine Pizza mit einem bestimmten Namen anzuzeigen
+
+def show_pizz_with_name (name):
+    for pizza in pizzas:
+        if pizza.name == name:
+            return pizza
+    return None
--- pizza.py
+++ pizza.py
@@ -1,5 +1,17 @@
 class Pizza:
+
+    #Bauplan/Konstruktor Pizza Klasse
+
     def __init__(self, name, topping, price):
         self.name = name
         self.topping = topping
         self.price = price
+
+#Funktion Informationen der Pizza anzeigen
+    def pizza_informations(self):
+        return self.name + "-" + self.topping + "-" + self.price + "CHF"
+
+#Funktion überprüfen der Pizzen nach Toppings
+
+    def has_topping(self, topping):
+        return topping in self.topping
